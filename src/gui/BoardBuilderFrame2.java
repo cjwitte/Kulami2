8cd1ef424e99f8113ea2063791b4d7eda82cd33e
Jon Witte <jwitte@mailbox.org>
2016-12-03 21:28:18 +0100

    One Panel, all Tiles, track location via x

---
+++ src/gui/BoardBuilderFrame2.java
@@ -0,0 +1,12 @@
+package gui;
+
+public class BoardBuilderFrame2 extends JFrame {
+	
+	private Board board;
+	private BoardPanel boardPanel;
+	RectForBuilder b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r;
+	
+	
+	
+
+}
